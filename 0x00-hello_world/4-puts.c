b02c16a484e72420361c78e62a5bad4cbc0b7b59
a7medfared <you@example.commdfared@gmail.com>
2023-03-16 18:09:12 -0700

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * return: Alawys 0 if exited properly, non-zero otherwise
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
