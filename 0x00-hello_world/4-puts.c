04f5c4ff4c83db1740eaf0555bb130f84e8b2159
a7medfared <you@example.commdfared@gmail.com>
2023-03-16 18:04:40 -0700

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * return: Alawys 0 (success)
+ * return: Alawys 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
